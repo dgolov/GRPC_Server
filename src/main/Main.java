af784985d7180c340c50088067f12c2548761397
dgolov <dmitriy.evgenyevich@gmail.com>
2024-08-17 02:35:41 +0300

    Added server

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,8 +1,22 @@
 package main;
 
+import io.grpc.Server;
+import io.grpc.ServerBuilder;
+
+import java.io.IOException;
+
+
 public class Main {
 
-    public static void main(String[] args) {
-	    System.out.println("Hello world");
+    public static void main(String[] args) throws IOException, InterruptedException {
+        Server server = ServerBuilder.forPort(8080)
+                .addService(new GreetingServiceImp())
+                .build();
+
+        server.start();
+
+        System.out.println("Server started");
+
+        server.awaitTermination();
     }
 }
